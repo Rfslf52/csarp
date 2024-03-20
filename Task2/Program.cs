ceb1bafd5cf59d02e697c2e64a16992e2eb4800d
Maksim_B. <rfslf@bk.ru>
2024-03-20 22:18:35 +0300

    dobavili delenie

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,11 @@
 int i2 = 6;
 int sum = i1 +i2;
 Console.WriteLine(sum);
+
+int quotion = i1/i2;
+Console.WriteLine(quotion);
+ double d1 = 5.5;
+
+ d1 = 6;
+ double quotion2 = i1/d1;
+ Console.WriteLine(quotion2);
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2b1585038c0f896d1cea4f12e0acee1078a8f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88893ca981adfbf6f3f47555042bed1e38e67fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
